c48ae101addf680dc02d126ede645254864560a6
carcorea <46208845+carcorea@users.noreply.github.com>
2019-01-21 17:51:49 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>settlements</title>
+  </head>
+  <style media="screen">
+  h1{
+   font-size:large;
+   text-align: center;
+   border-bottom:1px solid gray;
+   margin: 0;
+   padding: 20px;
+   font color:"blue";
+   }
+   ol{
+     border-right: 1px solid gray;
+     width: 100px;
+     margin: 0;
+     padding: 20px;
+   }
+  </style>
+  <body>
+    <h1>SETTLEMENNTS SHEET</h1>
+    <ul>
+      <ol>
+        월마감
+      </ol>
+      <ol>
+        낙찰평가
+      </ol>
+      <ol>
+        카코리아부품
+      </ol>
+
+      </ol>
+
+
+
+
+
+
+
+
+    </ul>
+
+  </body>
+</html>
